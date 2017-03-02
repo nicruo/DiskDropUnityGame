96bf78d8bfd9a6d8635602d29636a26e67d9cea2
José Medeiros <jmedeiros@agap2.pt>
2017-03-02 19:20:21 +0000

    First commit;

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+	private Rigidbody body;
+
+	public float speed = 30;
+
+	void Start () {
+		body = this.GetComponent<Rigidbody>();
+
+	}
+
+	void Update () {
+
+		float horizontal = Input.GetAxis ("Horizontal");
+		float vertical = Input.GetAxis ("Vertical");
+
+		body.AddForce (new Vector3(horizontal*speed,vertical*speed,0));
+
+		//Debug.Log (horizontal);
+	}
+
+}
